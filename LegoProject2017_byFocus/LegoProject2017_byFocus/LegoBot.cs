c3e5167f290c7d90b8b1ab9a1f55ce1f67bff931
sharmila8989 <sharmila8989@users.noreply.github.com>
2017-11-20 17:36:23 +1100

    added classes

---
+++ LegoProject2017_byFocus/LegoProject2017_byFocus/LegoBot.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lego.Ev3.Core;
+using Lego.Ev3.Desktop;
+
+namespace LegoProject2017_byFocus
+{
+    class LegoBot
+    {
+        private static MainBrick mainBrick;
+        private GyroSensor gyroSensor;
+        private ColourSensor colourSensor;
+        private UltrasonicSensor ultrasonicSensor;
+        private Motor motor;
+
+        public static MainBrick MainBrick
+        {
+            get
+            {
+                return mainBrick;
+            }
+            set
+            {
+                mainBrick = value;
+            }
+        }
+
+        public GyroSensor GyroSensor
+        {
+            get
+            {
+                return gyroSensor;
+            }
+            set
+            {
+                gyroSensor = value;
+            }
+        }
+
+        public ColourSensor ColourSensor
+        {
+            get
+            {
+                return colourSensor;
+            }
+            set
+            {
+                colourSensor = value;
+            }
+        }
+
+        public UltrasonicSensor UltrasonicSensor
+        {
+            get
+            {
+                return ultrasonicSensor;
+            }
+            set
+            {
+                ultrasonicSensor = value;
+            }
+        }
+
+        public Motor Motor
+        {
+            get
+            {
+                return motor;
+            }
+            set
+            {
+                motor = value;
+            }
+         
+        }
+
+        public LegoBot()
+        {
+            mainBrick = new MainBrick();
+            gyroSensor = new GyroSensor();
+            ultrasonicSensor = new UltrasonicSensor();
+            colourSensor = new ColourSensor();
+            motor = new Motor();
+
+
+        }
+
+        public async Task MoveForward()
+        {
+            await MainBrick.Brick.DirectCommand.TurnMotorAtPowerForTimeAsync(OutputPort.A, 40, 50, true);
+            await MainBrick.Brick.DirectCommand.TurnMotorAtPowerForTimeAsync(OutputPort.D, 40, 50, true);
+            await Task.Delay(10);
+        }
+        public async Task MoveBackward()
+        {
+            await MainBrick.Brick.DirectCommand.TurnMotorAtPowerForTimeAsync(OutputPort.A, -40, 10, false);
+            await MainBrick.Brick.DirectCommand.TurnMotorAtPowerForTimeAsync(OutputPort.D, -40, 10, false);
+            await Task.Delay(10);
+        }
+        public async Task Stop()
+        {
+            await MainBrick.Brick.DirectCommand.StopMotorAsync(OutputPort.A | OutputPort.D, true);
+        }
+
+        public async Task TurnLeft()
+        {
+            // delay it by few milli sec
+            await Task.Delay(100);
+
+            // store current angle
+            float currentAngle = gyroSensor.SIValue;
+            while(gyroSensor.SIValue <= currentAngle - 90)
+            {
+                MainBrick.Brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.A, -30, 10, false);
+                MainBrick.Brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.D, 30, 10, false);
+                await MainBrick.Brick.BatchCommand.SendCommandAsync();
+                await Task.Delay(10);
+            }
+        }
+
+        public async Task TurnRight()
+        {
+            // delay it by few milli sec
+            await Task.Delay(100);
+
+            // store current angle
+            float currentAngle = gyroSensor.SIValue;
+            while (gyroSensor.SIValue <= currentAngle + 90)
+            {
+                MainBrick.Brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.A, 30, 10, false);
+                MainBrick.Brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.D, -30, 10, false);
+                await MainBrick.Brick.BatchCommand.SendCommandAsync();
+                await Task.Delay(10);
+            }
+        }
+
+        public async Task turnAround()
+        {
+
+            await Task.Delay(100);
+            float currentAngle = GyroSensor.SIValue;
+
+            while (gyroSensor.SIValue >= currentAngle - 720)
+            {
+                MainBrick.Brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.A, -27, 10, false);
+                MainBrick.Brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.D, 27, 10, false);
+                await MainBrick.Brick.BatchCommand.SendCommandAsync();
+                await Task.Delay(100);
+            }
+        }
+
+
+        // position legobot for free movement
+        public async Task adjustPosition()
+        {
+            await Task.Delay(100);
+            
+            if( UltrasonicSensor.SIValue > 0 && UltrasonicSensor.SIValue < 10)
+            {
+                do { await MoveBackward(); }while(UltrasonicSensor.SIValue >= 20);
+            }
+            else if(UltrasonicSensor.SIValue > 20 && UltrasonicSensor.SIValue < 110)
+            {
+                do { await MoveForward(); } while (UltrasonicSensor.SIValue <= 20);
+            }
+            
+        }
+        
+    }
+}
---
+++ LegoProject2017_byFocus/LegoProject2017_byFocus/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lego.Ev3.Core;
+using Lego.Ev3.Desktop;
+using Lego;
+using Lego.Ev3;
+
+namespace LegoProject2017_byFocus
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        LegoBot legobot = new LegoBot();
+        public bool stopButtonClicked = false;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            //throw new NotImplementedException();
+
+            //legobot = new LegoBot();
+            await LegoBot.MainBrick.ConnectToBrick();
+            
+            await LegoBot.MainBrick.Brick.DirectCommand.ClearAllDevicesAsync();
+            LegoBot.MainBrick.Brick.BrickChanged += Brick_BrickChanged;
+
+
+                
+        }
+
+        private void  Brick_BrickChanged(object sender, BrickChangedEventArgs e)
+        {
+            //throw new NotImplementedException();
+            gyroTextBox.Text = legobot.GyroSensor.SIValue.ToString();
+            colourTextBox.Text = legobot.ColourSensor.SIValue.ToString();
+            ultraTextBox.Text = legobot.UltrasonicSensor.SIValue.ToString();
+           
+        }
+
+        private async void forwardButton_Click(object sender, RoutedEventArgs e)
+        {
+            //float distance = legobot.UltrasonicSensor.SIValue;
+            while (legobot.UltrasonicSensor.SIValue >= 10 )
+            {
+                await legobot.MoveForward();
+            } await legobot.Stop();
+            
+        }
+
+        private async void backwardButton_Click(object sender, RoutedEventArgs e)
+        {
+            //float distance = legobot.UltrasonicSensor.SIValue;
+            
+            for(int i=0; i < 200; i++)
+            { await legobot.MoveBackward(); }
+                
+            
+
+        }
+
+        private async void turnLeftButton_Click(object sender, RoutedEventArgs e)
+        {
+            await legobot.TurnLeft();
+
+        }
+
+        private async void turnRightButton_Click(object sender, RoutedEventArgs e)
+        {
+            await legobot.TurnRight();
+        }
+
+        private void turnAroundButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async void stopButton_Click(object sender, RoutedEventArgs e)
+        {
+            //stopButtonClicked = true;
+            await legobot.Stop();
+        }
+    }
+}
